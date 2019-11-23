04ad8b33b4a66563c489a779fb7e038467861d08
D0ot <gbcays@qq.com>
2019-11-23 19:44:23 +0800

    add some comments.

--- src/MyAgent/think.cc
+++ src/MyAgent/think.cc
@@ -11,11 +11,23 @@
 
 void MyAgent::think()
 {
+    // we can add action to cerebellum
+    // Actions will be encode to message which will be sent to server.
     auto& cerebellum = SCerebellum::getInstance();
+
+
+    // we can get raw data from cochlea
     auto& cochlea = SCochlea::getInstance();
+
+    // Basic infomation such as filed size, play mode, etc
     auto& worldmodel = SWorldModel::getInstance();
+
+    // All location infomation which needs algrithm can be get from localizer
     auto& localizer = SLocalizer::getInstance();
+
     auto& debugger = SDebugger::getInstance();
 
+
+    // commit the actions
     cerebellum.outputCommands(SAgentSocketComm::getInstance());
 }
